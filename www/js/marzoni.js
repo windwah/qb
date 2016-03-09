d497b55fc8f0d0f722f8dee7a75ab16c93e16874
windwah <windwahchan@gmail.com>
2016-03-10 03:27:14 +0800

    Update 1.0.7

--- www/js/marzoni.js
+++ www/js/marzoni.js
@@ -4,12 +4,12 @@ var marzoni = {
 	serverUrl: "http://58.64.129.183",
 	minQty:10,
 	avgQty:50,
-	marzoni_version: '1.0.6'
+	marzoni_version: '1.0.7'
 }
 
 function chaneLanguage(lang, cb){
 	localStorage.setItem('lang',lang);
-	var url = 'js/lang/' + lang + '.js'
+	var url = marzoni.serverUrl + '/js/lang/' + lang + '.js'
 	jQuery.getScript( url, cb );
 }
 
