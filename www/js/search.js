909ae77c13d9405f9cfa640d8b46aa49ff698dd7
Windwah <windwahchan@gmail.com>
2016-08-10 14:03:37 +0800

    Update

--- www/js/search.js
+++ www/js/search.js
@@ -201,7 +201,14 @@ var app = {
           }, 
           function (error) {
               alert("Scanning failed: " + error);
-          }
+          },
+		    {
+			  "preferFrontCamera" : false, // iOS and Android
+			  "showFlipCameraButton" : true, // iOS and Android
+			  "prompt" : "Place a barcode inside the scan area", // supported on Android only
+			  "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+			  "orientation" : "portrait" // Android only (portrait|landscape), default unset so it rotates with the device
+			}
        );
     },
 
